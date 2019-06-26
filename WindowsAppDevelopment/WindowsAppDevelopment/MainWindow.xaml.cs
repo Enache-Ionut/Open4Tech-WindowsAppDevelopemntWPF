9535f03b998f761cede4a2d1353ea25bda8c76e1
Enache Ionut <ionut.enache@caphyon.com>
2019-06-26 18:18:03 +0300

    First Day Project

--- WindowsAppDevelopment/WindowsAppDevelopment/MainWindow.xaml.cs
+++ WindowsAppDevelopment/WindowsAppDevelopment/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,64 @@ namespace WindowsAppDevelopment
   /// <summary>
   /// Interaction logic for MainWindow.xaml
   /// </summary>
-  public partial class MainWindow : Window
+  public partial class MainWindow : Window, INotifyPropertyChanged
   {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+
+    private string email;
+
+
+    public string LoginToUse { get; private set; } = "Login to use Open4tech";
+
+    public string Password { get; private set; }
+
+
+    public string Email
+    {
+      get
+      {
+        return email;
+      }
+      set
+      {
+        email = value;
+
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Email"));
+
+        //if (PropertyChanged != null)
+        //  PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Email"));
+      }
+    }
+
     public MainWindow()
     {
       InitializeComponent();
+      DataContext = this;
     }
+
+    private void LoginButton_Click(object sender, RoutedEventArgs e)
+    {
+      MessageBox.Show("Reset your password");
+
+      Email = string.Empty;
+
+      //this.Close();
+    }
+
+    private void ForgotPasswordButton_Click(object sender, RoutedEventArgs e)
+    {
+      MessageBox.Show("Reset your password");
+    }
+
+    private void OnPasswordChanged(object sender, RoutedEventArgs e)
+    {
+      if (DataContext != null)
+      {
+        Password = ((PasswordBox)sender).Password;
+      }
+
+    }
+
   }
 }
